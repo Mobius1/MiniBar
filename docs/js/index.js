1fae3716d0a7e0b820931576de6b377e3f740b67
Mobius1 <admin@twitchaddict.tv>
2017-07-18 13:15:17 +0100

    Updare DOCS

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,6 +1,8 @@
-var scrollers = [];
-[].slice.call(document.getElementsByClassName("content")).forEach(el => {
-	scrollers.push(new MiniBar(el, {
-		alwaysShowBars: "visible" in el.dataset
-	}));
+document.addEventListener("DOMContentLoaded", function(e) {
+	var scrollers = [];
+	[].slice.call(document.getElementsByClassName("content")).forEach(el => {
+		scrollers.push(new MiniBar(el, {
+			alwaysShowBars: "visible" in el.dataset
+		}));
+	});
 });
