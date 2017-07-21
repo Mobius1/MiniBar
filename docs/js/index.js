335e6c8a05939c5f8d99ad05a641b17cc9dcaaed
Karl <mobius1@gmx.com>
2017-07-21 16:26:49 +0100

    Update index.js

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,8 +1,16 @@
-document.addEventListener("DOMContentLoaded", function(e) {
+document.addEventListener("DOMContentLoaded", init);
+window.addEventListener("load", init);
+
+function init() {
 	var scrollers = [];
-	[].slice.call(document.getElementsByClassName("content")).forEach(el => {
+	[].slice.call(document.querySelectorAll(".content")).forEach((el, i) => {
 		scrollers.push(new MiniBar(el, {
-			alwaysShowBars: "visible" in el.dataset
+			alwaysShowBars: "visible" in el.dataset,
+			barType: i === 4 ? "progress" : "default"
 		}));
 	});
-});
+
+	new MiniBar(document.getElementsByTagName("main")[0], {
+		alwaysShowBars: true
+	});
+}
