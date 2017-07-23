5b836856fc3eb9c02b34fb25d176751813d139d8
Karl <mobius1@gmx.com>
2017-07-23 10:24:33 +0100

    Update index.js

--- docs/js/index.js
+++ docs/js/index.js
@@ -8,12 +8,44 @@ function init() {
 		alwaysShowBars: true
 	};
 	
-	[].slice.call(document.querySelectorAll(".inner")).forEach((el, i) => {
+	[].slice.call(document.querySelectorAll(".inner")).forEach(function(el, i) {
 		scrollers.push(new MiniBar(el, {
-			alwaysShowBars: "visible" in el.dataset,
+			alwaysShowBars: true,
 			barType: i === 5 ? "progress" : "default"
 		}));
 	});
 
 	new MiniBar(document.getElementsByTagName("main")[0]);
+	
+	document.querySelector(".tools").addEventListener("click", function(e) {
+		var t = e.target;
+		
+		if ( t.nodeName === "BUTTON" ) {
+			var action = t.getAttribute("data-action");
+			
+			switch(action) {
+				case "add":
+					add(scrollers[8]);
+					break;
+				case "remove":
+					remove(scrollers[8]);
+					break;					
+				default:
+					scrollers[8][action]();
+			}
+		}
+	});
+}
+
+function add(scroller) {
+	if ( scroller.initialised ) {
+		scroller.content.appendChild(scroller.content.firstElementChild.cloneNode(true));
+		scroller.update();
+	}
+}
+function remove(scroller) {
+	if ( scroller.initialised && scroller.content.childElementCount > 1 ) {
+		scroller.content.removeChild(scroller.content.lastElementChild);
+		scroller.update();
+	}
 }
