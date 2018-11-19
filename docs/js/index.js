dba7edd520de8a6556d27476c812a3e0b2442828
Mobius1 <admin@twitchaddict.tv>
2018-11-19 16:01:08 +0000

    Update DOCS

--- docs/js/index.js
+++ docs/js/index.js
@@ -10,8 +10,31 @@ function init() {
 
 	[].slice.call(document.querySelectorAll(".inner")).forEach(function(el, i, arr) {
 
-		if ( el.nodeName !== "TEXTAREA" && i < arr.length - 1 ) {
+		if ( el.nodeName !== "TEXTAREA" && i !== 7 ) {
 			el.firstElementChild.innerHTML = getContent();
+		}
+
+		if ( i === 7 ) {
+			el.firstElementChild.innerHTML = `<div class="item">1</div>
+												<div class="item">2</div>
+												<div class="item">3</div>
+												<div class="item">4</div>
+												<div class="item">5</div>
+												<div class="item">6</div>
+												<div class="item">7</div>
+												<div class="item">8</div>
+												<div class="item">9</div>
+												<div class="item">10</div>
+												<div class="item">11</div>
+												<div class="item">12</div>
+												<div class="item">13</div>
+												<div class="item">14</div>
+												<div class="item">15</div>
+												<div class="item">16</div>
+												<div class="item">17</div>
+												<div class="item">18</div>
+												<div class="item">19</div>
+												<div class="item">20</div>`;
 		}
 
 		var options = {
@@ -21,9 +44,7 @@ function init() {
 		};
 
 		if ( i === 7 ) {
-			options.navButtons = {
-				enabled: true
-			};
+			options.hideBars = true;
 		}
 
 		if ( i === 9 ) {
